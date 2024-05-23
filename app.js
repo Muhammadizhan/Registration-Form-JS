6037de6f9255f7cc808f488fefa2d85f7297df6a
Muhammadizhan <izhanayaz4@gmail.com>
2024-05-23 11:13:47 +0500

    modified JS

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const form = document.querySelector("#form");
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
+
+let usernameRegex = /^[a-zA-Z0-9_]{4,15}$/;
+let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+let passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$&*]).{8,}$/;
+// let cnicRegex = /^\d{5}-\d{7}-\d{1}$/;
